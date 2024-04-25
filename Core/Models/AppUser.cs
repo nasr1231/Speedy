f8aaec4a001e42afe7b1ca48fdaa4369f33db8aa
nasr1231 <nasr.1547474@gmail.com>
2024-04-25 20:37:52 +0200

    Build: Extending IdentityUser to AppUser table

--- Core/Models/AppUser.cs
+++ Core/Models/AppUser.cs
@@ -3,17 +3,27 @@
 namespace Speedy.Core.Models
 {
     public class AppUser : IdentityUser
-    {        
+    {
+        #region Properties
         public string FirstName { get; set; } = null!;
         public string LastName { get; set; } = null!;
-        public string Gender { get; set; }
-        public string Address { get; set; }
-        public string Age { get; set; }
-        public string? ProfilePictureIUrl { get; set; }
-        public DateTime RegistrationDate { get; set; }
-        public DateTime LastUpdateDate { get; set; }
-        public bool IsActive { get; set; }
-        public string NID { get; set; } = null!;
+        public string Gender { get; set; } = null!;        
+        public string Age { get; set; } = null!;
+        public string? ProfilePictureIUrl { get; set; }        
+        public double NID { get; set; }
+        #endregion
 
+        #region Conditions
+        public bool IsActive { get; set; }
+        public string? CreatedById { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.Now.ToUniversalTime();
+
+        public string? LastUpdatedById { get; set; }
+        public DateTime? LastUpdatedOn { get; set; }                
+        #endregion
+
+        #region Relations
+        public Delivery? Delivery { get; set; }        
+        #endregion
     }
 }
