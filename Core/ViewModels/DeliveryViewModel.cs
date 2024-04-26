001285cd0f3a9b4c2125999bc990564b2d3075a5
nasr1231 <nasr.1547474@gmail.com>
2024-04-26 19:40:47 +0300

    Add: Adding Delivery VM

--- Core/ViewModels/DeliveryViewModel.cs
+++ Core/ViewModels/DeliveryViewModel.cs
@@ -4,16 +4,21 @@ using System.ComponentModel.DataAnnotations;
 namespace Speedy.Core.ViewModels
 {
     public class DeliveryViewModel : BaseViewModel
-    {        
+    {
+        #region Properties
         public int FirstName { get; set; }
-        public int LastName { get; set; }
-        [Range(0,5)]
+        public int LastName { get; set; }        
         public Byte? Rate { get; set; }
         public string ServiceArea { get; set; } = null!;
         public string Address { get; set; } = null!;       
         public int ShippingMethodId { get; set; }
+        public string MobileNumber { get; set; } = null!;        
+        public string? Description { get; set; }
+        public bool HasWhatsApp { get; set; }
+        #endregion
 
+        #region Relations
         public ReviewViewModel? Reviews { get; }
-
+        #endregion
     }
 }
