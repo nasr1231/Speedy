6772e67f8c1a0a01f441d20dd9fcb54f36c7ba5d
nasr1231 <nasr.1547474@gmail.com>
2024-04-27 01:46:47 +0300

    [Add] adding user pages and errors

---
+++ Core/Consts/Errors.cs
@@ -0,0 +1,8 @@
+﻿namespace Speedy.Core.Consts
+{
+    public class Errors
+    {
+        public const string MaxLength = "It must be at max {0} characters long.";
+        public const string ConfirmPasswordMatch = "The password and confirmation password do not match.";
+    }
+}
