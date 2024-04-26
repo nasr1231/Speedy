cc71f5eff1a846d6fa6de8a1d514ac2433f7d9f1
nasr1231 <nasr.1547474@gmail.com>
2024-04-26 18:37:54 +0300

    Migration: Applying changes to db

---
+++ Data/Migrations/20240426143216_EditDeliveryTable.cs
@@ -0,0 +1,369 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Speedy.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditDeliveryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Deliveries_AspNetUsers_CreatedById",
+                table: "Deliveries");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Deliveries_AspNetUsers_LastUpdatedById",
+                table: "Deliveries");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_AspNetUsers_CreatedById",
+                table: "Reviews");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_AspNetUsers_LastUpdatedById",
+                table: "Reviews");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShippingMethods_AspNetUsers_CreatedById",
+                table: "ShippingMethods");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShippingMethods_AspNetUsers_LastUpdatedById",
+                table: "ShippingMethods");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ShippingMethods_CreatedById",
+                table: "ShippingMethods");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ShippingMethods_LastUpdatedById",
+                table: "ShippingMethods");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_CreatedById",
+                table: "Reviews");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_LastUpdatedById",
+                table: "Reviews");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Deliveries_CreatedById",
+                table: "Deliveries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Deliveries_LastUpdatedById",
+                table: "Deliveries");
+
+            migrationBuilder.DropColumn(
+                name: "ShippingMethod",
+                table: "Deliveries");
+
+            migrationBuilder.RenameColumn(
+                name: "ActiveStatus",
+                table: "Deliveries",
+                newName: "HasWhatsApp");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastUpdatedById",
+                table: "ShippingMethods",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedById",
+                table: "ShippingMethods",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastUpdatedById",
+                table: "Reviews",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedById",
+                table: "Reviews",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "Rate",
+                table: "Deliveries",
+                type: "tinyint",
+                nullable: true,
+                oldClrType: typeof(byte),
+                oldType: "tinyint");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastUpdatedById",
+                table: "Deliveries",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedById",
+                table: "Deliveries",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Deliveries",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MobileNumber",
+                table: "Deliveries",
+                type: "nvarchar(13)",
+                maxLength: 13,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NID",
+                table: "AspNetUsers",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(40)",
+                maxLength: 40,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(40)",
+                maxLength: 40,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Deliveries_MobileNumber",
+                table: "Deliveries",
+                column: "MobileNumber",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Deliveries_MobileNumber",
+                table: "Deliveries");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Deliveries");
+
+            migrationBuilder.DropColumn(
+                name: "MobileNumber",
+                table: "Deliveries");
+
+            migrationBuilder.RenameColumn(
+                name: "HasWhatsApp",
+                table: "Deliveries",
+                newName: "ActiveStatus");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastUpdatedById",
+                table: "ShippingMethods",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedById",
+                table: "ShippingMethods",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastUpdatedById",
+                table: "Reviews",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedById",
+                table: "Reviews",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "Rate",
+                table: "Deliveries",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0,
+                oldClrType: typeof(byte),
+                oldType: "tinyint",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastUpdatedById",
+                table: "Deliveries",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedById",
+                table: "Deliveries",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShippingMethod",
+                table: "Deliveries",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "NID",
+                table: "AspNetUsers",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(40)",
+                oldMaxLength: 40);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(40)",
+                oldMaxLength: 40);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShippingMethods_CreatedById",
+                table: "ShippingMethods",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShippingMethods_LastUpdatedById",
+                table: "ShippingMethods",
+                column: "LastUpdatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_CreatedById",
+                table: "Reviews",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_LastUpdatedById",
+                table: "Reviews",
+                column: "LastUpdatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Deliveries_CreatedById",
+                table: "Deliveries",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Deliveries_LastUpdatedById",
+                table: "Deliveries",
+                column: "LastUpdatedById");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Deliveries_AspNetUsers_CreatedById",
+                table: "Deliveries",
+                column: "CreatedById",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Deliveries_AspNetUsers_LastUpdatedById",
+                table: "Deliveries",
+                column: "LastUpdatedById",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_AspNetUsers_CreatedById",
+                table: "Reviews",
+                column: "CreatedById",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_AspNetUsers_LastUpdatedById",
+                table: "Reviews",
+                column: "LastUpdatedById",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShippingMethods_AspNetUsers_CreatedById",
+                table: "ShippingMethods",
+                column: "CreatedById",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShippingMethods_AspNetUsers_LastUpdatedById",
+                table: "ShippingMethods",
+                column: "LastUpdatedById",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
