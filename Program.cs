bc031b2ddaa8a8e2db484533c11c1d5ba8fb1cf6
nasr1231 <nasr.1547474@gmail.com>
2024-04-28 00:45:00 +0300

    fix

--- Program.cs
+++ Program.cs
@@ -28,11 +28,16 @@ public class Program
         builder.Services.AddControllersWithViews();
 
         var app = builder.Build();
-
+       
         // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseMigrationsEndPoint();
+        }
+        else
         {
             app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
 
