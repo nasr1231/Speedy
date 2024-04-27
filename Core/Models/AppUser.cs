c109ad9244d857308ce9a2df769b7d219c616224
nasr1231 <nasr.1547474@gmail.com>
2024-04-27 18:06:33 +0300

    format: formatting System Users page

--- Core/Models/AppUser.cs
+++ Core/Models/AppUser.cs
@@ -12,13 +12,13 @@ namespace Speedy.Core.Models
         public string FirstName { get; set; } = null!;
         [MaxLength(40)]
         public string LastName { get; set; } = null!;
-        public string Gender { get; set; } = null!;
+        public string Gender { get; set; } = string.Empty;
 
         [Range(16, 80)]
-        public byte Age { get; set; }
-        public string? ProfilePictureIUrl { get; set; }
+        public byte? Age { get; set; }
+        public string? ProfilePictureIUrl { get; set; } = string.Empty;
         [MaxLength(20)]
-        public string NID { get; set; } = null!;
+        public string NID { get; set; } = string.Empty;
         #endregion
 
         #region Conditions
--- Core/ViewModels/UserFormViewModel.cs
+++ Core/ViewModels/UserFormViewModel.cs
@@ -29,7 +29,7 @@ namespace Speedy.Core.ViewModels
 
         #region Roles
         [Display(Name = "Roles")]
-        public IList<int> SelectedRoles { get; set; } = new List<int>();
+        public IList<string> SelectedRoles { get; set; } = new List<string>();
         public IEnumerable<SelectListItem>? Roles { get; set; }
         #endregion
     }
