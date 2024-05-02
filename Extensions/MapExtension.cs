2ec9d70416e04b39b854d9dc804392bc14a34d16
nasr1231 <nasr.1547474@gmail.com>
2024-05-03 01:02:17 +0300

    add: adding extension method

---
+++ Extensions/MapExtension.cs
@@ -0,0 +1,19 @@
+﻿namespace ClinicGraduationProject.web.Extensions;
+
+public static class MapExtension
+{    
+    public static Delivery MapToDelivery(this DeliveryFormViewModel model)
+    {
+        var delivery = new Delivery
+        {
+            //Id = model.Id,
+            //LicenseNumber = model.LicenseNumber,
+            //MedicalSpecialtyId = model.MedicalSpecialtyId,
+            //AppUserId = model.UserId,
+            //CreatedById = model.UserId
+        };        
+
+        return delivery;
+    }
+
+}
