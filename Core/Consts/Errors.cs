0bf4aa74c1836abcbbf7bb2be03ad68a6e40848a
nasr1231 <nasr.1547474@gmail.com>
2024-05-05 00:55:21 +0300

    Add: adding const errors

--- Core/Consts/Errors.cs
+++ Core/Consts/Errors.cs
@@ -5,5 +5,7 @@
         public const string MaxLength = "It must be at max {0} characters long.";
         public const string ConfirmPasswordMatch = "The password and confirmation password do not match.";
         public const string isRequired = "{0} Field is required!";
+        public const string NotAllowedExtension = "يُسمح فقط بملفات .png، .jpg، .jpeg!";
+        public const string MaxSize = "لا يمكن أن يزيد حجم الملف عن 2 ميجابايت!";
     }
 }
