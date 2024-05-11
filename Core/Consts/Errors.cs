07cfcc2819fbf0df97fce680ea6a4ceaaec6fe11
nasr1231 <nasr.1547474@gmail.com>
2024-05-11 18:17:22 +0300

    change required message

--- Core/Consts/Errors.cs
+++ Core/Consts/Errors.cs
@@ -3,8 +3,8 @@
     public class Errors
     {
         public const string MaxLength = "It must be at max {0} characters long.";
-        public const string ConfirmPasswordMatch = "The password and confirmation password do not match.";
-        public const string isRequired = "{0} Field is required!";
+        public const string ConfirmPasswordMatch = "كلمتان المرور غير متطابقتين!!!";
+        public const string isRequired = "حقل {0} مطلوب!";
         public const string NotAllowedExtension = "يُسمح فقط بملفات .png، .jpg، .jpeg!";
         public const string MaxSize = "لا يمكن أن يزيد حجم الملف عن 2 ميجابايت!";
     }
