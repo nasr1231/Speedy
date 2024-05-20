7193fabc758349ce9999a56f41afea69cea31fda
nasr1231 <nasr.1547474@gmail.com>
2024-05-20 22:38:33 +0300

    add: adding JS validaitons for register forms

---
+++ wwwroot/js/register.js
@@ -0,0 +1,25 @@
+﻿function validateForm(event) {
+    var acceptTermsCheckbox = document.getElementById('AcceptTerms');
+    if (!acceptTermsCheckbox.checked) {
+        event.preventDefault();
+        alert('يجب أن توافق على الشروط والأحكام');
+    }
+}
+
+function updateHiddenGender() {
+    var selectedGender = document.querySelector('input[name="gender"]:checked').value;
+    document.getElementById('hidden-gender').value = selectedGender;
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var submitButton = document.getElementById('Submit-Btn');
+    submitButton.addEventListener('click', validateForm);
+
+    document.getElementById('radio-male').addEventListener('change', updateHiddenGender);
+    document.getElementById('radio-female').addEventListener('change', updateHiddenGender);
+
+    if (document.querySelector('input[name="gender"]:checked')) {
+        updateHiddenGender();
+    }    
+});
