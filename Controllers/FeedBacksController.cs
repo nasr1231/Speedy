66b9d664839368a9b9223f7cf51417761bc42815
nasr1231 <nasr.1547474@gmail.com>
2024-06-18 22:00:39 +0300

    FINAL: finishing feedback

--- Controllers/FeedBacksController.cs
+++ Controllers/FeedBacksController.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Speedy.Core.Models;
+using Speedy.Core.ViewModels;
+using System.Xml.Linq;
 
 namespace Speedy.Controllers
 {
-    public class FeedBacksController(ApplicationDbContext context, IMapper mapper) : Controller
+    public class FeedBacksController(ApplicationDbContext context, IMapper mapper, UserManager<AppUser> userManager) : Controller
     {
         private readonly ApplicationDbContext _context = context;
         private readonly IMapper _mapper = mapper;
+        private readonly UserManager<AppUser> _userManager = userManager;
         public IActionResult Index()
         {
             return View();
@@ -21,6 +27,7 @@ namespace Speedy.Controllers
             {
                 UserId = model.UserId,
                 Message = model.Message,
+                CreatedOn = DateTime.Now,
             };
 
             _context.Feedbacks.Add(feedbackMessage);
@@ -32,6 +39,46 @@ namespace Speedy.Controllers
             ViewBag.SuccessMessage = "تم تسجيل رأيك بنجاح";
 
             return View("Index");
+        }
+
+        public IActionResult Dashboard()
+        {
+            var feedbacks = _context.Feedbacks.Include(A => A.User).ToList();
+            var viewModels = new List<FeedbacksDashboardViewModel>();
+
+            foreach (var feed in feedbacks)
+            {
+                var viewModel = new FeedbacksDashboardViewModel
+                {
+                    CreationDate = feed.CreatedOn,
+                    Id = feed.Id,
+                    UserId = feed.UserId,
+                    UserName = $"{feed.User.FirstName} {feed.User.LastName}"
+                };
+
+                viewModels.Add(viewModel);
+            }
+
+            return View("Dashboard", viewModels);
+        }
+        public IActionResult Message(int id)
+        {
+            var message = _context.Feedbacks
+                .Include(ap => ap.User)
+                .SingleOrDefault(i => i.Id == id);
+
+            if (message is null)
+                return NotFound(message);
+
+            var feedViewModel = new FeedbackViewModel
+            {
+                CreatedOn = message.CreatedOn,
+                Message = message.Message,
+                UserId = message.UserId,
+                UserName = $"{message.User.FirstName} {message.User.LastName}"
+            };
+
+            return PartialView("_MessageForm", feedViewModel);
         }
 
     }
---
+++ Core/ViewModels/FeedbacksDashboardViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Speedy.Core.ViewModels
+{
+    public class FeedbacksDashboardViewModel
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
